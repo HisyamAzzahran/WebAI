4b25f03455aaa2715544193ab23ad814c3d46777
HisyamAzzahran <hisyamjunior898@gmail.com>
2025-05-02 02:58:34 +0700

    UpdateBossku

--- backend/app.py
+++ backend/app.py
@@ -444,4 +444,4 @@ def delete_user():
         conn.close()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=8080)
