a1df3c54aa97e5a71cdbabc6e090aad57539c1bc
HisyamAzzahran <hisyamjunior898@gmail.com>
2025-05-17 19:40:44 +0700

    updater

--- backend/app.py
+++ backend/app.py
@@ -40,6 +40,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     username = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     tokens = db.Column(db.Integer, default=10)
     is_premium = db.Column(db.Boolean, default=False)
 
